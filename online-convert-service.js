1f8db08395fe160ae6430b237d0ecc451dcf4efd
Roman <s91630277@gmail.com>
2015-10-06 02:59:10 +0300

    Rev

--- online-convert-service.js
+++ online-convert-service.js
@@ -7,24 +7,44 @@ function OnlineConvertService(options) {
 }
 
 OnlineConvertService.prototype.createJob = function(data, callback) {
+	var file = data.input;
 	var conversionTarget = data.conversion;
+	var filebasename = basename(file);
+	var input = [{
+		type: "upload",
+		filename: filebasename
+	}];
 	var conversion = [{
 		target: conversionTarget
 	}];
+	var attachObject = {};
+	attachObject[filebasename] = file;
 	var result = unirest
 		.post("https://api2.online-convert.com/jobs")
 		.type("json")
 		.header("X-Oc-Api-Key", this._apiKey)
-		.send({
-			conversion: conversion
-		})
+		.attach(attachObject)
 		.end(function(response) {
 			if (response.error) {
 				return callback(response.error);
 			}
 			callback(null, response.body);
 		});
-	return result;
 }
+
+// unirest.post('http://mockbin.com/request')
+// .header('Accept', 'application/json')
+// .field({
+//   'parameter': 'value'
+// })
+// .attach({
+//   'file': 'dog.png',
+//   'relative file': fs.createReadStream(path.join(__dirname, 'dog.png')),
+//   'remote file': unirest.request('http://google.com/doodle.png')
+// })
+// .end(function (response) {
+//   console.log(response.body);
+// })
+
 
 module.exports = OnlineConvertService;
